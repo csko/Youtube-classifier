780047859555bbd2ade97aef6d447df02a4e07c4
csko <cskornel@gmail.com>
2012-09-27 19:45:18 +0200

    Features changed.

--- features.py
+++ features.py
@@ -9,6 +9,7 @@ import sys
 import codecs
 
 from parse import load_data, PROJECT
+from readability.readability import ReadabilityTool
 
 from nltk.collocations import TrigramCollocationFinder
 from nltk.collocations import BigramCollocationFinder
@@ -23,12 +24,13 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 
-PRINT_COEFS = True
+PRINT_COEFS = False
+PRINT_ERRORS = True
 
 http_re = re.compile(r"((http|ftp|https):\/\/)?[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", re.IGNORECASE)
 oddities_re = re.compile(ur"(=|¡¿|·|\\|\^|~|…|“|”|ß|€)")
 tokenize2_re = re.compile(r"(\w+)([-\*.\\/#])+", re.UNICODE)
-# TODO: re: phone number
+phonenumber_re = re.compile(r"(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})")
 
 bad_words = set([line.strip().lower() for line in open(PROJECT + 'db/badwords-adult.txt')])
 
@@ -55,6 +57,7 @@ def parse_text(text):
     text = text.replace(u"`", "'")
 
     text = http_re.sub(" dummyhtml ", text)
+    text = phonenumber_re.sub(" dummyphone ", text)
 #    text = dotdot_re.sub(lambda m: "%s %s %s" % (m.group(1), m.group(2), m.group(4)), text)
 
 #    text = youre_re.sub("you're", text) # TODO: proper split
@@ -73,7 +76,7 @@ def create_features(X, user_data):
     for date, comment, user in X:
         feat = {}
 
-#        comment = comment.lower()
+        comment = comment.lower()
 
         comment = parse_text(comment)
 
@@ -84,12 +87,12 @@ def create_features(X, user_data):
             doc += wordtokenizer.tokenize(sent)
 
         for i, word in enumerate(doc):
-#            if doc[i] in bad_words:
-#                doc[i] = '_badword_'
+            if doc[i] in bad_words:
+                doc[i] = '_badword_'
             doc[i] = ps.stem(doc[i])
             doc[i] = wnl.lemmatize(doc[i])
-#            if doc[i] in bad_words:
-#                doc[i] = '_badword_'
+            if doc[i] in bad_words:
+                doc[i] = '_badword_'
             all_words[doc[i]] += 1
 
 #        trigram_finder = TrigramCollocationFinder.from_words(comment)
@@ -103,7 +106,6 @@ def create_features(X, user_data):
 #        feat.update(trigram)
         feat.update(bigram)
 
-
         text_vocab = set(w for w in doc if w.isalpha())
         unusual = text_vocab.difference(english_vocab)
         unusual_ratio = len(unusual) / len(text_vocab) if len(text_vocab) != 0 else -1.0
@@ -112,6 +114,7 @@ def create_features(X, user_data):
 
         has_bad_word = True
         for word in bad_words:
+            break # REMOVE THIS!
             if word in comment.lower():
                 break
         else:
@@ -122,13 +125,22 @@ def create_features(X, user_data):
         def c_none(x):
             return x if x is not None else "__None__"
 
+        readability = ReadabilityTool(comment)
+
+        read_feat = {}
+        for f, val in readability.analyzedVars.items():
+            if f != 'words':
+                read_feat["_" + f] = val
+        for test, val in readability.tests_given_lang['eng'].items():
+            read_feat["__" + test] = val(readability.text)
+
         feat['_AlwaysPresent'] = True
         feat['_word_num'] = len(doc)
-#        feat['_sent_num'] = len(sents)
+        feat['_sent_num'] = len(sents)
         feat['_word_var'] = len(set(doc)) / len(doc)
-#        feat['_sent_var'] = len(set(sents)) / len(sents)
+        feat['_sent_var'] = len(set(sents)) / len(sents)
         feat['_unusual_ratio'] = unusual_ratio
-#        feat['_username'] = user
+        feat['_username'] = user
         feat['_user_subcount'] = int(user_info['SubscriberCount'])
         feat['_user_friends'] = int(user_info['FriendsAdded'])
         feat['_user_favs'] = int(user_info['VideosFavourited'])
@@ -149,7 +161,8 @@ def create_features(X, user_data):
         feat['_user_has_location'] = 1 if user_info['Location'] is not None else 0
         feat['_user_has_hometown'] = 1 if user_info['Hometown'] is not None else 0
 #        feat['_user_last'] = user_info['LastWebAccess']
-        feat['_has_bad_word'] = has_bad_word
+#        feat['_has_bad_word'] = has_bad_word
+        feat.update(read_feat)
 
 #        print feat
         res.append(feat)
@@ -174,7 +187,8 @@ def main():
     videos, users, reviews = load_data()
 
     print "Extracting features."
-    feats = create_features([(x['date'], x['text'], x['user']) for x in reviews], users)
+    orig_X = np.array([(x['date'], x['text'], x['user']) for x in reviews])
+    feats = create_features(orig_X, users)
     #y = np.array([1 if x['spam'] == 'true' else 0 for x in reviews])
     y = np.array([1 if x['adult'] == 'true' else 0 for x in reviews])
 
@@ -192,14 +206,36 @@ def main():
         cls.fit(feats, y)
         c = v.inverse_transform(cls.coef_)
         for key, val in sorted(c[0].iteritems(), key=lambda x: x[1]):
-            if isinstance(key, str) and key.startswith("_"):
-                print key, val
+#            if isinstance(key, str) and key.startswith("_"):
+             print key, val
         quit()
 
-    scores = cross_validation.cross_val_score(cls, feats, y, cv=10, score_func=metrics.f1_score)
-    for i, score in enumerate(scores):
-        print "Fold %d: %.5f" % (i, score)
-    print "Mean score: %0.5f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
+    f1sum = 0
+    for i, (train_idx, test_idx) in enumerate(cv):
+        train_X, train_y, test_X, test_y = feats[train_idx], \
+                y[train_idx], feats[test_idx], y[test_idx]
+        cls.fit(train_X, train_y)
+        preds = cls.predict(test_X)
+
+        if PRINT_ERRORS:
+#            worst = np.argsort(np.abs(test_y - preds))
+            #for j in worst[-1:-10:-1]:
+            orig_test = orig_X[test_idx]
+#            for j in worst:
+            for j in range(len(orig_test)):
+                if test_y[j] != preds[j]:
+                    print j, orig_test[j], test_y[j], preds[j]
+            #quit()
+
+        f1 = metrics.f1_score(test_y, preds)
+        print "Fold %d F1 score: %.5f" % (i, f1)
+        f1sum += f1
+    print "Mean F1 score: %.5f" % (f1sum / k)
+
+#    scores = cross_validation.cross_val_score(cls, feats, y, cv=10, score_func=metrics.f1_score)
+#    for i, score in enumerate(scores):
+#        print "Fold %d: %.5f" % (i, score)
+#    print "Mean score: %0.5f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
 
 
 if __name__ == "__main__":
